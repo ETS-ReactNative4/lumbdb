4851c39c45ae358db2a0ecc795b17c0d4009ac04
ErnieBuyco <ernie.buyco@fmclarity.com>
2018-10-11 15:05:10 +0800

    hi there

--- src/App.js
+++ src/App.js
@@ -2,27 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import Movie from './Movie';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  }, {
+    id: 2,
+    title: 'Spider Man'
+
+  },  {
+    id: 3,
+    title: '36th Chamber of Shaolin'
+  }, {
+    id: 4,
+    title: 'Five deadly Venoms'
+  }
+];
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         </header>
+          {movies.map(movie => <Movie key={movie.id} movie={movie} /> )}
       </div>
     );
   }
 }
+
 
 export default App;
